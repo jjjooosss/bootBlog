deacf04d108af2b22f6118f100e3d098c3190d2e
jjjooosss <suuuu3030@gmail.com>
2023-07-30 23:15:38 +0900

    delete test. dummy test 종료

---
+++ src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.cos.blog.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+
+//ControllerAdvice : 이 컨트롤러가 어떤 클래스에서 exception이 발생하든 처리 가능하다
+@ControllerAdvice
+@RestController
+public class GlobalExceptionHandler {
+//    모든 에러를 스프링이 감지해서 이 메소드로 보내줌
+    @ExceptionHandler(value = Exception.class)
+    public String handleArgumentException(Exception e){
+        return "<h1>"+e.getMessage()+"</h1>";
+    }
+
+
+}
--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -5,12 +5,14 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -21,16 +23,56 @@ public class DummyControllerTest {
     @Autowired//의존성주입(DI)
     private UserRepository userRepository;
 
-    @GetMapping("/dummy/user")
+    @DeleteMapping("/dummy/user/{id}")
+    public String delete(@PathVariable int id){
+        try {
+        userRepository.deleteById(id);
+//        } catch (Exception e){
+//            좀 더 정확한 exception 확인
+        } catch (EmptyResultDataAccessException e){
+            return "삭제에 실패하였습니다. 해당 id는 db에 없습니다.";
+        }
+        return "삭제되었습니다. id :"+id;
+    }
+
+//    save함수는 id를 전달하지 않으면 insert를 해주고,
+//    id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고,
+//    id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 해준다.
+//    email, password
+    @Transactional
+    @PutMapping("/dummy/user/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User requestUser){
+        //RequestBody : json데이터를 요청 => java object(메시지컨버터의 jackson라이브러리가 변환해서 받아줌)
+        System.out.println("id = " + id + ", password = " + requestUser.getPassword());
+        System.out.println("email" + requestUser.getEmail());
+
+        User user = userRepository.findById(id).orElseThrow(()->{
+            return new IllegalArgumentException("수정에 실패하였습니다.");
+        });
+
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+
+//        userRepository.save(user);
+//        더티체킹
+        return user; // 업데이트 결과를 볼 수 있도록
+    }
+
+    @GetMapping("/dummy/users")
     public List<User> list(){
         return userRepository.findAll();
     }
 
-    @GetMapping("/dummy/user/page/{page}")
+    @GetMapping("/dummy/user")
 //    스프링 부트의 자동 페이징기능
 //    2건씩 들고오고, id기준 최신순 정렬
-    public Page<User> pageList(@PageableDefault(size = 2, sort = "id",direction = Sort.Direction.DESC)Pageable pageable) {
-        Page<User> users = userRepository.findAll(pageable);
+    public List<User> pageList(@PageableDefault(size = 2, sort = "id",direction = Sort.Direction.DESC)Pageable pageable) {
+//        나중에 여기서 로직 추가 가능
+        Page<User> pagingUser = userRepository.findAll(pageable);
+
+//        getContent : 페이징 완료한 컨텐츠만 보임
+        List<User> users = pagingUser.getContent();
+//        return users;
         return users;
     }
 //    {id} 주소로 파라미터를 전달 받을 수 있음.
